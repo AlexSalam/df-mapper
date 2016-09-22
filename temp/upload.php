d9bc834724fb30060f81b0f4909ee17783f63fe7
=Alex Salam <vornat2@gmail.com>
2016-09-22 16:55:55 +0100

    Uploaded MIN complete

--- temp/upload.php
+++ temp/upload.php
@@ -12,22 +12,28 @@
     "msg" => array()
   );
 
-  if ($_FILES['type'] != "application/x-gtar") {
+  if ($_FILES['bmp-tar']['type'] != "application/x-gzip") {
     $error['error'] = true;
-    array_push($error['msg'], "Upload of incorrect MIME type, Uploads must be GNU tar archives.");
+    array_push($error['msg'], "Upload of incorrect MIME type, Uploads must be GNU zip archives.");
   }
   if ($_FILES['size'] === 0 || $_FILES['size'] > 5000000) {
     $error['error'] = true;
     array_push($error['msg'], "Upload too large, you are limited to 50mb.");
   }
-
-  die;
-  if ($error['error'] === true) {
+  if ($error['error']) {
     $returnString = "";
     foreach ($error['msg'] as $msg) {
       $returnString .= urlencode($msg) . '&';
     }
     header('location: ../index.php?' . $returnString);
+  } else {
+
+    //If no errors process upload
+
+    move_uploaded_file($_FILES['bmp-tar']['tmp_name'], "map.tar.gz");
+
+
+    header('location: ../index.php');
   }
 
 ?>
